6501a21979e5c01425b60dfe9779f15787ff679a
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-03-15 21:07:55 +0530

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f'Hi, {name}')
+    # Press F9 to toggle the breakpoint.
+    print("vaishu")
 
 
 # Press the green button in the gutter to run the script.
