d1f2788b88e653cc6ea3f5326c55a96ded8e2265
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-03-16 23:33:00 +0530

    learned code execution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("1")
 # This is a sample Python script.
 
 # Press Ctrl+F5 to execute it or replace it with your code.
@@ -6,13 +7,33 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    print(name)
+    print("3")
     print(f'Hi, {name}')
     # Press F9 to toggle the breakpoint.
     print("vaishu")
+    a = 5
+    print(a)
+    b = 6
+    a = b
+    print(a,'b')
+    print(b)
+    b = 7
+    print(b)
+    result = a*b
+    print("Multiplication of 5 and 6 is: 3000",result)
+
+def get_name(name):
+    print(name)
 
 
 # Press the green button in the gutter to run the script.
+print("5")
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print("2")
+    print_hi('vaishu')
+    print("***")
+    get_name('ravi mama')
+    print("4")
+print("8")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
